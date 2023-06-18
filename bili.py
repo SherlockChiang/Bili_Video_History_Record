7a033d8f9517eaa87443dbb5fcd1d5b93c12db36
Chiang Yuhan <98642231+SherlockChiang@users.noreply.github.com>
2023-06-18 13:52:21 +0800

    Update bili.py
    更新
    爬虫没有办法获取部分category的详细tag，故整合之
    使用tag导出tensor

--- bili.py
+++ bili.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 # 这里参考了https://blog.csdn.net/qq_18303993/article/details/114481841
 import qrcode
 from threading import Thread
@@ -19,6 +20,30 @@ def get_key(val, dict):
     for key, value in dict.items():
          if val in value:
              return key
+
+def get_index(val, cate, keys_cate, dict):
+    index = 0
+    if cate == '番剧':
+        index = 104
+    elif cate == '电影':
+        index = 105
+    elif cate == '电视剧':
+        index = 106
+    elif cate == '纪录片':
+        index = 107
+    elif cate == '国创':
+        index = 108
+    elif cate == '综艺':
+        index = 109
+    elif cate in keys_cate:
+        t = keys_cate.index(cate)
+        for i in range(t):
+            index = index + len(dict[keys_cate[i]])
+        list_tmp = dict[cate]
+        index = index + list_tmp.index(val) + 1
+    else:
+        index = 110
+    return index
 
 requests.packages.urllib3.disable_warnings()
 
@@ -126,7 +151,7 @@ for file_name in os.listdir(data_dir):
         fpath = os.path.join(data_dir, file_name)
         print(fpath)
         df_ = pd.read_excel(fpath)
-        df = df.append(pd.read_excel(fpath))
+        df = df._append(pd.read_excel(fpath))
  
 # 数据预处理
 df.drop_duplicates(inplace=True)
@@ -135,11 +160,7 @@ df['date'] = pd.to_datetime(df['view_at'],unit='s',origin=pd.Timestamp('1970-01-
 df['date'] = df['date'].dt.strftime("%Y-%m-%d")
 df = df.fillna({"tag_name":"无"})
 
-
 category = {
- '番剧': ['资讯', '官方延伸','连载动画','完结动画''新番时间表','番剧索引'],
- '国创': ['国产动画', '国产原创相关', '布袋戏', '资讯', '动态漫·广播剧','新番时间表','国产动画索引'],
- '综艺': ['免费','大会员'],
  '动画': ['MAD·AMV', 'MMD·3D', '综合', '短片·手书·配音', '手办·模玩', '特摄', '动漫杂谈'],
  '鬼畜': ['鬼畜调教', '音MAD', '人力VOCALOID', '鬼畜剧场', '教程演示'],
  '舞蹈': ['宅舞', '舞蹈综合', '舞蹈教程', '街舞', '明星舞蹈', '中国舞', '国风舞蹈', '手势·网红舞'],
@@ -148,9 +169,6 @@ category = {
  '美食': ['美食制作', '美食侦探', '美食测评', '田园美食', '美食记录'],
  '汽车': ['汽车生活', '赛车','改装玩车','新能源车','房车','摩托车','购车攻略'],
  '运动': ['篮球','足球','健身', '竞技体育', '运动文化', '运动综合'],
- '电影': ['其他国家', '欧美电影', '日本电影', '国产电影'],
- '电视剧': ['国产剧', '海外剧'],
- '纪录片': ['人文·历史', '科学·探索·自然', '军事', '社会·美食·旅行'],
  '游戏': ['单机游戏', '网络游戏', '手机游戏', '电子竞技', '桌游棋牌', '音游', 'GMV', 'Mugen', '游戏赛事'],
  '音乐': ['音乐综合', '音乐现场', '演奏', '翻唱', 'MV', 'VOCALOID·UTAU', '电音', '原创音乐', '乐评盘点', '音乐教学', '说唱'],
  '影视': ['小剧场', '影视杂谈', '影视剪辑', '预告·资讯'],
@@ -159,7 +177,12 @@ category = {
  '生活': ['搞笑','亲子','出行','三农','家居房产','手工','绘画','日常'],
  '时尚': ['美妆护肤', '穿搭', '时尚潮流', '仿妆cos'],
  '动物圈': ['喵星人', '汪星人', '野生动物', '爬宠', '大熊猫', '动物综合', '小宠异宠', '动物二创'],
- '直播': ['聊天电台' ,'游戏直播', '虚拟主播']
+ '番剧': ['资讯', '官方延伸','连载动画','完结动画', '新番时间表','番剧索引'],
+ '电影': ['其他国家', '欧美电影', '日本电影', '国产电影'],
+ '电视剧': ['国产剧', '海外剧'],
+ '纪录片': ['人文·历史', '科学·探索·自然', '军事', '社会·美食·旅行'],
+ '国创': ['国产动画', '国产原创相关', '布袋戏', '资讯', '动态漫·广播剧','新番时间表','国产动画索引'],
+ '综艺': ['免费','大会员'],
 }
 
 tmp = []
@@ -179,6 +202,7 @@ for i in range(num):
 
 df['category'] = tmp
 
+# 按序排序
 category_list = category.keys()
 cat_size_order = CategoricalDtype(
     category_list, 
@@ -189,11 +213,15 @@ df['category'] = df['category'].astype(cat_size_order)
 df2 = pd.DataFrame() 
 df2 = df['category'].sort_values().value_counts(sort=False)
 
+# get index
+indexes = [0] * 110
+for i in range(num):
+    indexes[get_index(tag_lists[i],tmp[i],list(category_list),category)-1] += 1
+
 workbook = xlwt.Workbook(encoding = 'utf-8')
 worksheet = workbook.add_sheet('sheet1')
-tensor = df2.tolist()
 ii = 0
-for i in tensor:
+for i in indexes:
     worksheet.write(0,ii,i)
     ii+=1
 workbook.save('tensor.xls')
@@ -205,3 +233,4 @@ plt.title('您近三个月中的b站浏览视频类型分析')
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 plt.show() 
+
